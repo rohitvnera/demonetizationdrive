6ad502d90fb862a2504619b29c730b40d570dbdd
aviundefined <iitk.avinash.nigam@gmail.com>
2016-11-14 20:21:04 +0530

    remove key from chart api

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -306,7 +306,7 @@ jQuery(function($) {
 
     function showMap(inputLatLong){
         var locationData = {};
-        var imageUrl = 'https://chart.apis.google.com/chart?cht=mm&chs=24x32&chco=FFFFFF,008CFF,000000&ext=.png&key=AIzaSyCJG1h0f41Jv3RRZz6ZjzpnUfLBFJDt8zQ';
+        var imageUrl = 'https://chart.apis.google.com/chart?cht=mm&chs=24x32&chco=FFFFFF,008CFF,000000&ext=.png';
         var markerImage = new google.maps.MarkerImage(imageUrl,new google.maps.Size(24, 32));
         if (!inputLatLong) {
             var addresFromSearch = $('#address').val();

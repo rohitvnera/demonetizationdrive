d9045143b00215e4cb4ff6f7e2a48db4b6c5af3c
Ajinkya Harkare <ajinkya@vnera.com>
2016-11-16 14:12:47 +0530

    Removing debugger

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -25,7 +25,6 @@ jQuery(function($) {
 
 
     $(function(){
-        debugger;
         firstLoad();
         $('#okBtn').on('click', onOk);
         $('#updateBankButton').on('click', onUpdateModal);
